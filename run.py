85e5cee0b01a687f7c18d39e17d62737bcbdfd36
im5033 <59259635+im5033@users.noreply.github.com>
2024-06-27 19:47:26 +0800

    Update run.py

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ DEBUG_MODE = False  # Debug模式，是否打印请求返回信息
 # PROXY = input('请输入代理，如不需要直接回车:')  # 代理，如果多次出现IP问题可尝试将自己所用的魔法设置为代理。例如：使用clash则设置为 'http://127.0.0.1:7890'
 PROXY = ''
 INVITE_CODE = os.getenv('INVITE_CODE') or input('请输入邀请码: ')
-WEBHOOK_URL = os.getenv('WEBHOOK_URL') or "https://discordapp.com/api/webhooks/1255843702497345677/Y23jdw_LfOnYJfxCBunZ_gwCcCerse9ksudvhGoFg5UbWBf1Iqgoq4fsrjOMPtBSnOQ-"
+WEBHOOK_URL = "https://discordapp.com/api/webhooks/1255843702497345677/Y23jdw_LfOnYJfxCBunZ_gwCcCerse9ksudvhGoFg5UbWBf1Iqgoq4fsrjOMPtBSnOQ-"
 
 # 检查变量
 def check_env():
@@ -33,7 +33,7 @@ def send_discord_message(webhook_url, message):
     data = {
         "content": message
     }
-    response = requests.post("https://discordapp.com/api/webhooks/1255843702497345677/"+webhook_url, headers=headers, data=json.dumps(data))
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
     
     if response.status_code != 204:
         print(f"Failed to send message to Discord. Status code: {response.status_code}")
